9784adeaff5c3b151636fb8ecb0a1972192f9d7d
Yoshiori SHOJI <yoshiori@gmail.com>
2022-02-03 21:38:44 +0900

    :memo: chapter 2 finish

--- ch2/ch2.py
+++ ch2/ch2.py
@@ -34,6 +34,12 @@ def OR(x1, x2):
     return 1
 
 
+def XOR(x1, x2):
+  s1 = NAND(x1, x2)
+  s2 = OR(x1, x2)
+  y = AND(s1, s2)
+  return y
+
 print("AND")
 print(AND(0, 0))
 print(AND(1, 0))
@@ -51,3 +57,9 @@ print(OR(0, 0))
 print(OR(1, 0))
 print(OR(0, 1))
 print(OR(1, 1))
+
+print("XOR")
+print(XOR(0, 0))
+print(XOR(1, 0))
+print(XOR(0, 1))
+print(XOR(1, 1))
